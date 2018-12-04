05354912d6c1542ae4aefa55d73f6a52fc726209
kiranbhalerao123 <bhaleraok65@gmail.com>
2018-12-04 12:26:33 +0530

    apollo-client added to project to fetch projects from Github graphql-api

--- src/index.js
+++ src/index.js
@@ -2,6 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { ApolloClient } from "apollo-client";
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { ApolloProvider } from 'react-apollo';
+import config from "./config/dev";
 
+const client = new ApolloClient({
+  link: new HttpLink({
+    uri: "https://api.github.com/graphql",
+    credentials: "include",
+    headers: {
+      authorization: `Bearer ${config.AUTH_TOKEN}`
+    }
+  }),
+  cache: new InMemoryCache()
+});
+const ApolloApp = AppComponent => (
+  <ApolloProvider client={client}>
+    <AppComponent />
+  </ApolloProvider>
+);
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(ApolloApp(App), document.getElementById("root"));
