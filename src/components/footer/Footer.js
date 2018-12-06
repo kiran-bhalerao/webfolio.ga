3c67cb7994111400be2d0e10dc9af21e9d4944e0
kiranbhalerao123 <bhaleraok65@gmail.com>
2018-12-06 21:08:00 +0530

    footer text changed

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -7,12 +7,12 @@ class Footer extends Component {
         style={{ background: "#fff" }}
         id="footer"
       >
-        <p className="mb-1 pb-0">
+        <p className="mb-0 pb-0">
           <span className="text-danger">{"<3"}</span>{" "}
           <strong>Open Source</strong>
         </p>
-        <p className="text-center p-3">
-          Copyright © 2018 Designed and Developed by <strong>Kiran Bhalerao</strong>.
+        <p className="text-center pl-3 pr-3 pt-1">
+          Copyright © 2018 Created by <strong>Kiran Bhalerao</strong>.
         </p>
       </div>
     );
